0fa7939bb95dc2b84faa66e8956b706a70b1442b
pranaliandre <pranaliandre06@gmail.com>
2020-03-25 09:12:49 -0500

    Added: Calculated wages till total hours or days is reached

--- empWagesComputation.sh
+++ empWagesComputation.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -x
-
+B
 #CONSTANTS
 EMP_RATE_PER_HOUR=20
 EMP_FULL_DAY_HOUR=8
@@ -8,9 +8,11 @@ IS_PRESENT=1
 FULL_TIME_EMP=2
 PART_TIME_EMP=1
 NUM_OF_WORKING=20
+TOTAL_WORKING_HRS=100
 #variables
 wageForADay=0
-totalWage
+totalWage=0
+empWorkingHours=0
 echo "Welcome to Employee wages computation"
 function dailyEmpWage(){
    echo $(($1*$2))
@@ -30,7 +32,7 @@ function empCaseStatement(){
 			wageForADay=0
 			;;
 		esac
-}	
+}
 empCaseStatement
 
 #function to calculate employee type, Full time or Part time
@@ -62,10 +64,30 @@ function wagesForAMonth(){
 			wageForADay=0
    	fi
 		echo $wageForADay
-		totalWage=$((totalWage+wageForADay)) #calculate the total salary of month
+		totalWage=$((totalWage+wageForADay)) #calculate the total salary for month
 	done
 }
 wagesForAMonth
+
+#calculates wages till total hours or days is reached
+while [[ $empWorkingHours -lt $TOTAL_WORKING_HRS && $dayCount -lt $NUM_OF_WORKING_DAYS ]]
+do
+	((dayCount++))
+	local empCheck=$((RANDOM%3))
+   case $empCheck in
+      $EMP_FULL_DAY_HOUR)
+         wageForADay=$( dailyEmpWage $EMP_RATE_PER_HOUR $EMP_FULL_DAY_HOUR )
+         ;;
+      $EMP_PART_DAY_HOUR)
+         wageForADay=$( dailyEmpWage  $EMP_RATE_PER_HOUR $EMP_PART_DAY_HOUR )
+         ;;
+      *)
+         wageForADay=0
+         ;;
+      esac
+	empWorkingHours=$((empWorkingHours+wageForADay))
+totalSalary=$(( $empWorkingHours*$EMP_RATE_PER_HOUR ))#calculate the  works hours or days
+done
 
 #function to calulate employee wages
 # param1 : wages per hour
